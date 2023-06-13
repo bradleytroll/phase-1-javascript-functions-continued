67bdb512880f362f54f76227c54a31d5a9897d19
Bradley Troll <bradleytroll@gmail.com>
2023-06-13 10:43:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+//Redid the lab for extra practice. New code below; original code commented out.
+
 function saturdayFun(activity = 'roller-skate') {
-    return (`This Saturday, I want to ${activity}!`)
+  return `This Saturday, I want to ${activity}!`
 }
+
 function mondayWork(activity = 'go to the office') {
-    return (`This Monday, I will ${activity}.`)
+  return `This Monday, I will ${activity}.`
 }
 function wrapAdjective(symbol) {
     return function(adjective) {
       return `You are ${symbol}${adjective}${symbol}!`;
     }
   }
+
+// function saturdayFun(activity = 'roller-skate') {
+//     return (`This Saturday, I want to ${activity}!`)
+// }
+// function mondayWork(activity = 'go to the office') {
+//     return (`This Monday, I will ${activity}.`)
+// }
+// function wrapAdjective(symbol) {
+//     return function(adjective) {
+//       return `You are ${symbol}${adjective}${symbol}!`;
+//     }
+//   }
